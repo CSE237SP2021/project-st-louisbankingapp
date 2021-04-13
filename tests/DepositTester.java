03822ab5b0107817998450be9fae7a53ac25a059
Richard Zaldivar <rmzaldivar@wustl.edu>
2021-04-13 16:50:22 -0500

    Added withdraw function to account and a corresponding unit test. Also updated deposit function and depositTester to be more rigorous.

--- tests/DepositTester.java
+++ tests/DepositTester.java
@@ -8,8 +8,10 @@ Account depositTester = new Account();
 	@Test
 	void testDeposit() {
 		assertEquals(0.0, depositTester.deposit(0.0));
-		assertEquals(50.25, depositTester.deposit(50.25));
-		assertEquals(80.25, depositTester.deposit(30.0));
+		assertEquals(50.2, depositTester.deposit(50.2));
+		assertEquals(80.2, depositTester.deposit(30.0));
+		assertEquals(80.2, depositTester.deposit(-30.2));
+		assertEquals(180.2, depositTester.deposit(100));
 	}
 
 }
